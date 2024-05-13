fd88f41e5c0f4c13a56aa54883aab0c95c522e52
kaskor-z <kaskor.z@yandex.ru>
2024-05-14 00:13:53 +0700

    Add files via upload
    Задание от 10.05 "Строки и их индексация"

---
+++ ASK_M01_Less_01.py
@@ -0,0 +1,8 @@
+string_1 = 'это новая строка'
+string_2 = 'это моя строка'
+print(string_2[0])
+print(string_2[-1])
+print(string_2[2:5])
+print(string_2[::-1])
+print(len(string_2))
+print(string_2,string_1)
