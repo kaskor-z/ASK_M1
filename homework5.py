9e425ca8e5feffff81da3e5716d341a6134189a9
kaskor-z <kaskor.z@yandex.ru>
2024-05-17 02:28:51 +0700

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,10 @@
+my_list = ['orange', 'banana', 'apple', 'tangerine', 'pear']
+print(my_list)
+print(my_list[0], my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'prune'
+print(my_list)
+my_dict = {'orange':'апельсин', 'banana':'банан', 'apple':'яблоко', 'tangerine':'мандарин', 'pear':'груша'}
+print(my_dict)
+my_dict.update({'apple':'Очень сладкое яблоко', 'prune':'слива'})
+print(my_dict)
