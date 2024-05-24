c977a2bc5b9d6b9a1a93372ecceb19d2e5106a61
kaskor-z <kaskor.z@yandex.ru>
2024-05-24 19:21:41 +0700

    Update ASK_M01_Less_02.py
    Условия задания были изменены после сбоя в работе платформы URBAN (сбой 14.05.2024).

--- ASK_M01_Less_02.py
+++ ASK_M01_Less_02.py
@@ -1,6 +1,21 @@
-num1 = 5
-num2 = 3
-sum = num1 + num2
-print(sum)
-result = sum + num1 * num2
-print(result)
+# Код выполнения задания по устаревшим условиям
+
+# um1 = 5
+# num2 = 3
+# sum = num1 + num2
+# print(sum)
+# result = sum + num1 * num2
+# print(result)
+#
+# Код выполнения задания по обновлённым условиям
+#
+namber_of_homework = 75
+number_of_hours_spent = 36
+training_course_name = 'Python'
+hours_spent_per_session = number_of_hours_spent // namber_of_homework
+add_minit_spent_per_session = round((number_of_hours_spent % namber_of_homework) /
+                               namber_of_homework * 60)
+print('Курс: "', training_course_name, '" - всего задач:', namber_of_homework,
+      '; затрачено часов:', number_of_hours_spent,
+      '; среднее время выполнения:', hours_spent_per_session, 'часов ',
+      add_minit_spent_per_session, 'минут.')
